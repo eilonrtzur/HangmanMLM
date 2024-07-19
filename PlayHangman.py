82d461597e0dcbdec4ee612c804403911930ff55
Eilon <eilontzur@gmail.com>
2024-07-18 17:38:07 -0700

    Updated user input possibilities for playing and improved function typing.

--- PlayHangman.py
+++ PlayHangman.py
@@ -1,14 +1,16 @@
 import string
+from string import ascii_letters
 import os
 import pickle
 import numpy as np
 import torch
 import torch.nn as nn
-from CreateData import TrainData, encode_function
+from typing import Callable
+from CreateData import TrainData, encode_function, filter_dictionary, build_dictionary
 from TrainModels import Head, MultiHeadAttention, Block, FeedFoward, PositionalEncoding, TransformerHangman
 np.set_printoptions(precision=2,suppress=True)
 
-def decode_function():
+def decode_function() -> Callable[[list[int]], str]:
     """ Defines a decode function which turns an array of numbers into a string of characters """
     chars = [letter for letter in string.ascii_lowercase]
     chars.insert(0,'.')
@@ -16,7 +18,7 @@ def decode_function():
     decode = lambda l: ''.join([itos[i] for i in l]) # decoder: take a list of integers, output a string
     return decode
 
-def load_models(save_files: list[str]):
+def load_models(save_files: list[str]) -> list[TransformerHangman]:
     """ loads trained models """
     models = []
     for save_file in save_files:
@@ -24,7 +26,7 @@ def load_models(save_files: list[str]):
         models.append(pickle.load(file))
     return models
 
-def model_output(model, status: str):
+def model_output(model: TransformerHangman, status: str) -> torch.Tensor:
     """ evaluates a model on a given input string representing the status of the hangman game and converts to probabilities """
     m = nn.Softmax(dim=2)
     encode = encode_function()
@@ -34,19 +36,19 @@ def model_output(model, status: str):
     output = m(output)
     output = torch.sum(output,1)
     output = torch.squeeze(output)
-    result = output.detach().numpy() 
+    result = output.detach()
     result[0]=0
     return result
 
-def remove_guessed(model_output, guessed_letters: list[int]):
+def remove_guessed(model_output: torch.Tensor, guessed_letters: list[int]) -> torch.Tensor:
     # sets probabilities of guessed letters to 0 and renormalizes
     for num in guessed_letters:
         model_output[num] = 0
-    if sum(model_output) != 0:
-        model_output = model_output/sum(model_output) 
+    if torch.sum(model_output) != 0:
+        model_output = model_output/torch.sum(model_output) 
     return model_output
 
-def generate_prediction(models,guessed_letters,status, weights):
+def generate_prediction(models: list[TransformerHangman], guessed_letters: list[str], status: str, weights: torch.Tensor) -> str:
     """ generate next letter prediction based on current status and previously guessed letters """
     encode = encode_function()
     decode = decode_function()
@@ -56,8 +58,10 @@ def generate_prediction(models,guessed_letters,status, weights):
     logits.append(remove_guessed(model_output(models[0].eval(), status[:3]),guessed_letters))
     logits.append(remove_guessed(model_output(models[1].eval(), status[-3:]),guessed_letters))
     for j in range(2,min(len(models),length)): # create a loop for n-gram models
-        logits.append(remove_guessed(sum(model_output(models[j].eval(),status[i:i+j+1]) for i in range(length-j)),guessed_letters)/(length-j))
-    result = sum(weights[i]*logits[i] for i in range(len(logits)))
+        logits.append(remove_guessed(torch.sum(torch.stack([model_output(models[j].eval(),status[i:i+j+1]) for i in range(length-j)]),dim=0),guessed_letters)/(length-j))
+    logits = torch.stack(logits)
+    result = torch.matmul(weights,logits)
+    result = result.numpy()
     prediction = (-result).argsort()
     prediction = prediction.tolist()
     return decode([prediction[0]])
@@ -67,14 +71,14 @@ def update_word(status: str, word: str, letter: str) -> str:
     new_status = ''.join([status[i] if word[i]!= letter else letter for i in range(len(status))])
     return new_status
 
-def PlayHangman(word, weight = None, mode = None) -> int:
+def PlayHangman(word: str, weight: torch.Tensor = None, mode: str = None) -> int:
     """ 
     simulates an entire game of hangman for chosen word and component weights
     if mode is 'print', it will print out responses for each round of letter guessing in game
     returns 1 for a win and 0 for a loss 
     """
     if weight == None:
-        weight = [1 for i in range(len(word))]
+        weight = torch.ones(min(len(word),8))
     else:
         weight = weight[:len(word)]
     guessed_letters = []
@@ -120,7 +124,7 @@ def PlayHangman(word, weight = None, mode = None) -> int:
             print('You lose! The word was', word)
         return 0
 
-def WinRate(sample: list[str], weight = None) -> float:
+def WinRate(sample: list[str], weight: torch.Tensor = None) -> float:
     """ returns percentage of games won for a given sample of words """
     games_won = 0
     games_played = len(sample)
@@ -149,14 +153,25 @@ if __name__ == '__main__':
     Running this script will prompt the user to enter either "game" or "win-rate". If "game" is entered, then it will ask for a word. 
     If "win-rate" is entered, it will ask for a sample size. 
     """
+
     model_files = ['prefix_model.pkl', 'suffix_model.pkl', '3gram_model.pkl', '4gram_model.pkl', '5gram_model.pkl', '6gram_model.pkl', '7gram_model.pkl', '8gram_model.pkl']
     models = load_models(model_files)
     response = input('Choose game or win-rate: ')
     if response == 'game':
-        word = input('Choose a word: ')
-        PlayHangman(word,None,'print')
+        word = input('Choose a word or type random please for a random word: ')
+        word = word.lower()
+        if word != 'random please':
+            if set(word).difference(ascii_letters):
+                print('Your word must only include english letters, no special characters are allowed.')
+            else:    
+                PlayHangman(word,None,'print')
+        else:
+            PlayHangman(create_sample('allwords.pkl',1)[0],None,'print')
     elif response == 'win-rate':
         sample_size = int(input('How big a sample would you like? '))
-        print('Your win rate is ',WinRate(create_sample('allwords.pkl',sample_size)))
+        if sample_size < 1:
+            print('You entered an invalid sample size.')
+        else:
+            print('Your win rate is ',WinRate(create_sample('allwords.pkl',sample_size)))
     else: 
         print('That is not an option. Please choose type game or win-rate next time.')
