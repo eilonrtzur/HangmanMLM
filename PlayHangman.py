f78f7c6f1482568f67fd8de682a6a2b160f3e762
Eilon <eilontzur@gmail.com>
2024-07-23 10:57:48 -0700

    replaced weights features with MoE model, moved load_models to MixtureofExperts.py

--- PlayHangman.py
+++ PlayHangman.py
@@ -8,6 +8,7 @@ import torch.nn as nn
 from typing import Callable
 from CreateData import TrainData, encode_function, filter_dictionary, build_dictionary
 from TrainModels import Head, MultiHeadAttention, Block, FeedFoward, PositionalEncoding, TransformerHangman
+from MixtureofExperts import MoE, load_models
 np.set_printoptions(precision=2,suppress=True)
 
 def decode_function() -> Callable[[list[int]], str]:
@@ -18,13 +19,11 @@ def decode_function() -> Callable[[list[int]], str]:
     decode = lambda l: ''.join([itos[i] for i in l]) # decoder: take a list of integers, output a string
     return decode
 
-def load_models(save_files: list[str]) -> list[TransformerHangman]:
-    """ loads trained models """
-    models = []
-    for save_file in save_files:
-        file = open(os.path.join('models', save_file),"rb")
-        models.append(pickle.load(file))
-    return models
+def load_MoE(save_file: str) -> MoE:
+    """ loads MoE model """
+    file = open(os.path.join('models', save_file),"rb")
+    model = pickle.load(file)
+    return model
 
 def model_output(model: TransformerHangman, status: str) -> torch.Tensor:
     """ evaluates a model on a given input string representing the status of the hangman game and converts to probabilities """
@@ -40,15 +39,25 @@ def model_output(model: TransformerHangman, status: str) -> torch.Tensor:
     result[0]=0
     return result
 
+def moe_output(model: MoE, status: str) -> torch.Tensor:
+    """ evaluates MoE model and outputs weights """
+    encode = encode_function()
+    for i in range(20-len(status)):
+        status += '_'
+    padded_word = torch.Tensor(encode(status))
+    padded_word = padded_word[None,:]
+    output = model(padded_word)
+    output = output.squeeze().detach()
+    return output
+        
 def remove_guessed(model_output: torch.Tensor, guessed_letters: list[int]) -> torch.Tensor:
     # sets probabilities of guessed letters to 0 and renormalizes
     for num in guessed_letters:
         model_output[num] = 0
-    if torch.sum(model_output) != 0:
-        model_output = model_output/torch.sum(model_output) 
+
     return model_output
 
-def generate_prediction(models: list[TransformerHangman], guessed_letters: list[str], status: str, weights: torch.Tensor) -> str:
+def generate_prediction(models: list[TransformerHangman], moe_model: MoE, guessed_letters: list[str], status: str) -> str:
     """ generate next letter prediction based on current status and previously guessed letters """
     encode = encode_function()
     decode = decode_function()
@@ -58,8 +67,11 @@ def generate_prediction(models: list[TransformerHangman], guessed_letters: list[
     logits.append(remove_guessed(model_output(models[0].eval(), status[:3]),guessed_letters))
     logits.append(remove_guessed(model_output(models[1].eval(), status[-3:]),guessed_letters))
     for j in range(2,min(len(models),length)): # create a loop for n-gram models
-        logits.append(remove_guessed(torch.sum(torch.stack([model_output(models[j].eval(),status[i:i+j+1]) for i in range(length-j)]),dim=0),guessed_letters)/(length-j))
+        logits.append(remove_guessed(torch.sum(torch.stack([model_output(models[j].eval(),status[i:i+j+1]) for i in range(length-j)]),dim=0),guessed_letters)/(length-j))  
     logits = torch.stack(logits)
+    weights = moe_output(moe_model.eval(),status)[:logits.size(0)]
+    print(status, weights)
+    # weights = torch.ones(logits.size(0))
     result = torch.matmul(weights,logits)
     result = result.numpy()
     prediction = (-result).argsort()
@@ -71,33 +83,20 @@ def update_word(status: str, word: str, letter: str) -> str:
     new_status = ''.join([status[i] if word[i]!= letter else letter for i in range(len(status))])
     return new_status
 
-def PlayHangman(word: str, weight: torch.Tensor = None, mode: str = None) -> int:
+def play_hangman(word: str, mode: str = None) -> int:
     """ 
     simulates an entire game of hangman for chosen word and component weights
     if mode is 'print', it will print out responses for each round of letter guessing in game
     returns 1 for a win and 0 for a loss 
     """
-    if weight == None:
-        weight = torch.ones(min(len(word),8))
-    else:
-        weight = weight[:len(word)]
     guessed_letters = []
     incorrect_guesses = 0
-    global component_success
-
     status = ''
     for i in range(len(word)):
         status += '.'
 
     while status != word and incorrect_guesses<7:
-        if mode == 'weight_train':
-            (letter, components_array) = generate_prediction(models,guessed_letters,status, weight)
-            for i in range(len(components_array)):
-                if components_array[i] in word:
-                    component_success[i]+=1
-                
-        else:
-            letter = generate_prediction(models,guessed_letters,status, weight)
+        letter = generate_prediction(models,moe_model,guessed_letters,status)
         guessed_letters.append(letter)
         if letter in word:
             status = update_word(status,word,letter)
@@ -124,12 +123,12 @@ def PlayHangman(word: str, weight: torch.Tensor = None, mode: str = None) -> int
             print('You lose! The word was', word)
         return 0
 
-def WinRate(sample: list[str], weight: torch.Tensor = None) -> float:
+def win_rate(sample: list[str]) -> float:
     """ returns percentage of games won for a given sample of words """
     games_won = 0
     games_played = len(sample)
     for word in sample:
-        if PlayHangman(word, weight)==1:
+        if play_hangman(word)==1:
             games_won += 1
     return games_won/games_played
 
@@ -156,6 +155,7 @@ if __name__ == '__main__':
 
     model_files = ['prefix_model.pkl', 'suffix_model.pkl', '3gram_model.pkl', '4gram_model.pkl', '5gram_model.pkl', '6gram_model.pkl', '7gram_model.pkl', '8gram_model.pkl']
     models = load_models(model_files)
+    moe_model = load_MoE('MoE.pkl')
     response = input('Choose game or win-rate: ')
     if response == 'game':
         word = input('Choose a word or type random please for a random word: ')
@@ -164,14 +164,14 @@ if __name__ == '__main__':
             if set(word).difference(ascii_letters):
                 print('Your word must only include english letters, no special characters are allowed.')
             else:    
-                PlayHangman(word,None,'print')
+                play_hangman(word,'print')
         else:
-            PlayHangman(create_sample('allwords.pkl',1)[0],None,'print')
+            play_hangman(create_sample('allwords.pkl',1)[0],'print')
     elif response == 'win-rate':
         sample_size = int(input('How big a sample would you like? '))
         if sample_size < 1:
             print('You entered an invalid sample size.')
         else:
-            print('Your win rate is ',WinRate(create_sample('allwords.pkl',sample_size)))
+            print('Your win rate is ',win_rate(create_sample('allwords.pkl',sample_size,1)))
     else: 
         print('That is not an option. Please choose type game or win-rate next time.')
